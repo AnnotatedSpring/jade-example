1b977f8f5175bca8ca519099d672c064c8877dee
Brian Kelly <polymonic@gmail.com>
2015-09-15 17:41:16 -0400

    Initial commit

---
+++ src/main/java/demo/JadeTestApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JadeTestApplication.class, args);
+    }
+}
